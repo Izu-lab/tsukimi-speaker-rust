855540a31daf7555feec7d73a157abe921206e2d
kaido-atsuya <kaido.atsuya@gmail.com>
2025-09-23 23:34:21 +0900

    feat: ビルド
    Former-commit-id: a02cf4b27736e1360f29389523f5d00da394a9f6

--- build.rs
+++ build.rs
@@ -1,6 +1,13 @@
+use std::env;
 use tonic_prost_build::configure;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // 環境変数 `SKIP_PROTOC` が設定されている場合は、何もしないで終了
+    if env::var("SKIP_PROTOC").is_ok() {
+        println!("cargo:warning=Skipping protoc compilation because SKIP_PROTOC is set.");
+        return Ok(());
+    }
+
     configure()
         .out_dir("src/proto")
         .compile_protos(
